839b90cb9cf2eb652060559e1239a87eb5bbea1d
Amudi Sebastian <amudi@amudi.org>
2010-07-09 19:47:17 +0800

    added test case for issue #24 [completed: 24]

--- FASTFaceRecognizer/Tests/FASTFaceRecognizerTest.m
+++ FASTFaceRecognizer/Tests/FASTFaceRecognizerTest.m
@@ -8,6 +8,8 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 #import <UIKit/UIKit.h>
+#import "UIImage+Resize.h"
+#import "UIImage+Grayscale.h"
 #import "FaceTemplate.h"
 #import "FaceRecognizer.h"
 
@@ -27,6 +29,9 @@
 - (void)testFaceTemplateCreate;
 - (void)testFaceTemplateDealloc;
 - (void)testFaceTemplateLoadResource;
+
+// Issue #24
+- (void)testFaceTemplateLoadAndGetDistance;
 
 @end
 
@@ -89,6 +94,41 @@
 	STAssertTrue(sizeof(ft->pixelInfo) > 0, @"load resource failed, sizeof(ft->pixelInfo) = %d", sizeof(ft->pixelInfo));
 	STAssertTrue(ft->areaSize.width > 0, @"load resource resulted in a 0 width, areaSize.width = %d", (int)ft->areaSize.width);
 	STAssertTrue(ft->areaSize.height > 0, @"load resource resulted in a 0 height, areaSize.height = %d", (int)ft->areaSize.height);
+}
+
+// Issue #24
+- (void)testFaceTemplateLoadAndGetDistance {
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	
+	NSString *templatePathString = [bundle pathForResource:@"face_template" ofType:@"txt"];
+	const char *templatePath = [templatePathString cStringUsingEncoding:NSUTF8StringEncoding];
+	FaceTemplateLoadResource(ft, templatePath);
+	
+	NSString *photo1Path = [bundle pathForResource:@"photo1" ofType:@"jpg"];
+	UIImage *photo1 = [[UIImage alloc] initWithContentsOfFile:photo1Path];
+	UIImage *photo1Grayscale = [photo1 convertToGrayscale];
+	UIImage *photo1Resized = [photo1Grayscale resizedImage:CGSizeMake(80.0f, 60.0f) interpolationQuality:kCGInterpolationHigh]; 
+	CGImageRef photo1CGImage = [photo1Resized CGImage];
+	FaceRecognizer *fr1 = FaceRecognizerCreate(photo1CGImage, ft);
+	
+	NSString *photo2Path = [bundle pathForResource:@"photo2" ofType:@"jpg"];
+	UIImage *photo2 = [[UIImage alloc] initWithContentsOfFile:photo2Path];
+	UIImage *photo2Grayscale = [photo2 convertToGrayscale];
+	UIImage *photo2Resized = [photo2Grayscale resizedImage:CGSizeMake(80.0f, 60.0f) interpolationQuality:kCGInterpolationHigh]; 
+	CGImageRef photo2CGImage = [photo2Resized CGImage];
+	FaceRecognizer *fr2 = FaceRecognizerCreate(photo2CGImage, ft);
+	
+	long distance = -1;
+	distance = FaceRecognizerGetDistance(fr1, fr2);
+	STAssertTrue(distance != 1, @"failed to get distance, distance = %d", distance);
+	STAssertTrue(distance != 0, @"failed to get distance, distance = %d", distance);
+	STAssertTrue(distance < LONG_MAX, @"failed to get distance, distance = %d", distance);
+	STAssertTrue(distance > LONG_MIN, @"failed to get distance, distance = %d", distance);
+	
+	FaceRecognizerDealloc(fr1);
+	FaceRecognizerDealloc(fr2);
+	[photo1 release];
+	[photo2 release];
 }
 
 - (void)tearDown {

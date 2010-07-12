a8cf0a667d2a5f5a410d611847606393c1473735
Amudi Sebastian <amudi@amudi.org>
2010-07-12 11:46:51 +0800

    add FaceDistance for distance to percentage conversion [touch: 29]

---
+++ FASTFaceRecognizer/Classes/FaceDistance.c
@@ -0,0 +1,84 @@
+﻿//
+//  FaceDistance.c
+//  FASTFace
+//
+//  Created by Amudi Sebastian on 7/12/10.
+//  Copyright 2010 amudi.org. All rights reserved.
+//
+
+#include "FaceDistance.h"
+
+int FaceDistanceGetSamePersonPct(const long distance) {
+	if (distance < 21000L) {
+		return (int)((distance / (-210L)) + 99L);
+	} else {
+		return 1;
+	}
+}
+
+int FaceDistanceGetRelativesPct(const long distance) {
+	if (distance < 21000L) {
+		return (int)(((distance * distance) / (-4410000L)) + 99L);
+	} else {
+		return 1;
+	}
+}
+
+int FaceDistanceGetSoulmatePct(const long distance) {
+	if (distance < 21000L) {
+		return (int) (((distance * distance) / (-980000L)) + (distance / 70L) + 50L);
+	} else {
+		return 1;
+	}
+}
+
+int FaceDistanceGetAncestorPct(const long distance) {
+	if (distance < 21000L) {
+		return (int) (((distance * distance * 5) / (11L * 1000000L)) - (distance * 105L / 11000L) + 99L);
+	} else {
+		return 1;
+	}
+}
+
+int FaceDistanceGetCharacteristicPct(const long distance) {
+	if (distance < 21000L) {
+		return (int)((distance * distance) / 4410000L);
+	} else {
+		return 99;
+	}
+}
+
+FaceDistance *FaceDistanceCreateFaceDistance() {
+	FaceDistance *fd = (FaceDistance *)malloc(sizeof(FaceDistance));
+	fd->samePersonPct = 0;
+	fd->relativesPct = 0;
+	fd->soulmatePct = 0;
+	fd->ancestorPct = 0;
+	fd->characteristicPct = 0;
+	return fd;
+}
+
+FaceDistance *FaceDistanceCreate(const long distance) {
+	FaceDistance *fd = (FaceDistance *)malloc(sizeof(FaceDistance));
+	FaceDistanceGetDistance(fd, distance);
+	return fd;
+}
+
+void FaceDistanceDealloc(FaceDistance *fd) {
+	if (fd) {
+		free(fd);
+	}
+	fd = NULL;
+}
+
+void FaceDistanceGetDistance(FaceDistance *fd, const long distance) {
+	if (!fd) {
+		fd = CreateFaceDistance(distance);
+	}
+	fd->samePersonPct = GetSamePersonPct(distance);
+	fd->relativesPct = GetRelativesPct(distance);
+	fd->soulmatePct = GetSoulmatePct(distance);
+	fd->ancestorPct = GetAncestorPct(distance);
+	fd->characteristicPct = GetCharacteristicPct(distance);
+}
+
---
+++ FASTFaceRecognizer/Classes/FaceDistance.h
@@ -0,0 +1,31 @@
+﻿//
+//  FaceDistance.h
+//  FASTFace
+//
+//  Created by Amudi Sebastian on 7/12/10.
+//  Copyright 2010 amudi.org. All rights reserved.
+//
+
+#ifndef FACEDISTANCE_H
+#define FACEDISTANCE_H
+
+struct FaceDistance {
+	int samePersonPct;	// 1-100
+	int relativesPct;	// 1-100
+	int soulmatePct;	// 1-100
+	int ancestorPct;	// 1-100
+	int characteristicPct;	// 1-100
+};
+typedef struct FaceDistance FaceDistance;
+
+int FaceDistanceGetSamePersonPct(const long distance);
+int FaceDistanceGetRelativesPct(const long distance);
+int FaceDistanceGetSoulmatePct(const long distance);
+int FaceDistanceGetAncestorPct(const long distance);
+int FaceDistanceGetCharacteristicPct(const long distance);
+FaceDistance *FaceDistanceCreate();
+FaceDistance *FaceDistanceCreate(const long distance);
+void FaceDistanceDealloc(FaceDistance *fd);
+void FaceDistanceGetDistance(FaceDistance *fd, const long distance);
+
+#endif
